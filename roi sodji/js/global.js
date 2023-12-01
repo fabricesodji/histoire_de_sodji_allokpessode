ff98b5f63a5983a042bd9932c5840aa4f71da98d
fabricesodji <152660080+fabricesodji@users.noreply.github.com>
2023-12-01 23:14:30 +0100

    Add files via upload

---
+++ roi sodji/js/global.js
@@ -0,0 +1,11 @@
+let barnavBtn = document.getElementById("nav-barnav");
+let bugerBtn = document.getElementById("div-buger");
+
+
+function menuMobile() {
+    const btn = document.querySelector(".buger");
+    const header = document.querySelector(".header");
+    btn.addEventListener("click", () ⇨ {
+       conso
+    })
+} 
